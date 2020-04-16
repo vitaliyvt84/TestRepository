a21f0ad54cb97ff267ab50daeac8c20b10efedbb
Vitaliy Tarasenko <vitaliy.vt84@gmail.com>
2020-04-16 18:22:55 +0300

    Test commit

---
+++ src/main/java/com/students/web/HelloWorldServlet.java
@@ -0,0 +1,25 @@
+package com.students.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("<H1>Hello, world! или Привет мир</H1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
